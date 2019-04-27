6d3390b86dea3639b3a5dfbe47e5dd36285acce6
HeyMikeMarshall <mjmarshall@gmail.com>
2019-04-27 01:51:29 -0400

    map legends finished, map final versions.

---
+++ zcta_map/legendhtml.py
@@ -0,0 +1,302 @@
+emp_legend = f"""    
+    <div class='my-legend'>
+        <div class='legend-title'>Healthcare Locations and Unemployment Rate in the D.C. Metropolitan Area</div>
+        <hr>
+        <div class='legend-marker'>
+            <span style="color: black; font-size: 12; vertical-align: bottom;">&#9679;</span> - Healthcare Locations (Doctors and Hospital Facilities)</div>
+            
+            <div class='legend-scale'>
+            <ul class='legend-labels'>
+            <li><span style= width:400px;font-size:110%;text-align:left;>Unemployment Rate Color Scale:</span></li><br>
+            <li><span style='background:#006837;'></span>0%</li>
+            <li><span style='background:#199750;'></span>2%</li>
+            <li><span style='background:#66bd63;'></span>4%</li>
+            <li><span style='background:#a5d86a;'></span>6%</li>
+            <li><span style='background:#d9ef8b'></span>8%</li>
+            <li><span style='background:#feffbe;'></span>10%</li>
+            <li><span style='background:#fee08b;'></span>12%</li>
+            <li><span style='background:#fdad60;'></span>14%</li>
+            <li><span style='background:#f46d43;'></span>16%</li>
+            <li><span style='background:#d62f27'></span>18%</li>
+            <li><span style='background:#a50026'></span>&ge;20%</li>
+        </ul>
+        </div>
+        <div class='legend-source'>Sources: <a href="http://www.census.gov">US Census Data</a>, <a href="http://www.google.com">Google</a></div>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        <style type='text/css'>
+        .my-legend .legend-title {{
+            text-align: left;
+            margin-bottom: 8px;
+            font-weight: bold;
+            font-size: 150%;
+            }}
+        .my-legend .legend-marker {{
+            text-align: left;
+            margin-bottom: 10px;
+            font-size: 80%;
+            }}
+        .my-legend .legend-scaledsc {{
+            font-size: 90%;
+            padding: 0px;
+            }}
+    
+        .my-legend .legend-scale ul {{
+            margin-bottom: 0;
+            padding: 0;
+            float: left;
+            list-style: none;
+            }}
+        .my-legend .legend-scale ul li {{
+            display: block;
+            width: 40px;
+            margin-bottom: 0px;
+            float: left;
+            text-align: center;
+            font-size: 80%;
+            list-style: none;
+            }}
+        .my-legend ul.legend-labels li span {{
+            display: block;
+            float: left;
+            height: 15px;
+            width: 40px;
+            }}
+        .my-legend ul.legend-labels span {{
+            display: block;
+            height: 15px;
+            width: 80px;
+            float: left;
+            }}
+            
+            
+        .my-legend .legend-source {{
+            padding-top:10px;
+            font-size: 70%;
+            color: #999;
+            clear: both;
+            }}
+        .my-legend a {{
+            color: #777;
+            }}
+        </style>
+
+
+
+
+"""
+
+
+ins_legend = f"""
+     <div class='my-legend'>
+        <div class='legend-title'>Healthcare Locations and Insurance Coverage Rates in the D.C. Metropolitan Area</div>
+        <hr>
+        <div class='legend-marker'>
+            <span style="color: black; font-size: 12; vertical-align: bottom;">&#9679;</span> - Healthcare Locations (Doctors and Hospital Facilities)</div>
+            
+
+            <div class='legend-scale'>
+            <ul class='legend-labels'>
+            <li><span style= width:400px;font-size:110%;text-align:left;>Insurance Coverage Rate Color Scale:</span></li><br>
+            <li><span style='background:#a50026;'></span>0%</li>
+            <li><span style='background:#d62f27;'></span>10%</li>
+            <li><span style='background:#f46d43;'></span>20%</li>
+            <li><span style='background:#fdad60;'></span>30%</li>
+            <li><span style='background:#fee08b;'></span>40%</li>
+            <li><span style='background:#feffbe;'></span>50%</li>
+            <li><span style='background:#d9ef8b;'></span>60%</li>
+            <li><span style='background:#a5d86a;'></span>70%</li>
+            <li><span style='background:#66bd63;'></span>80%</li>
+            <li><span style='background:#199750;'></span>90%</li>
+            <li><span style='background:#006837;'></span>100%</li>
+        </ul>
+        </div>
+        <div class='legend-source'>Sources: <a href="http://www.census.gov">US Census Data</a>, <a href="http://www.google.com">Google</a></div>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        <style type='text/css'>
+        .my-legend .legend-title {{
+            text-align: left;
+            margin-bottom: 8px;
+            font-weight: bold;
+            font-size: 150%;
+            }}
+        .my-legend .legend-marker {{
+            text-align: left;
+            margin-bottom: 10px;
+            font-size: 80%;
+            }}
+        .my-legend .legend-scaledsc {{
+            font-size: 90%;
+            padding: 0px;
+            }}
+    
+        .my-legend .legend-scale ul {{
+            margin-bottom: 0;
+            padding: 0;
+            float: left;
+            list-style: none;
+            }}
+        .my-legend .legend-scale ul li {{
+            display: block;
+            width: 40px;
+            margin-bottom: 0px;
+            float: left;
+            text-align: center;
+            font-size: 80%;
+            list-style: none;
+            }}
+        .my-legend ul.legend-labels li span {{
+            display: block;
+            float: left;
+            height: 15px;
+            width: 40px;
+            }}
+        .my-legend ul.legend-labels span {{
+            display: block;
+            height: 15px;
+            width: 80px;
+            float: left;
+            }}
+            
+            
+        .my-legend .legend-source {{
+            padding-top:10px;
+            font-size: 70%;
+            color: #999;
+            clear: both;
+            }}
+        .my-legend a {{
+            color: #777;
+            }}
+        </style>
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+inc_legend = f"""    
+    
+    <div class='my-legend'>
+        <div class='legend-title'>Healthcare Locations and Per Capita Income in the D.C. Metropolitan Area</div>
+        <hr>
+        <div class='legend-marker'>
+            <span style="color: black; font-size: 12; vertical-align: bottom;">&#9679;</span> - Healthcare Locations (Doctors and Hospital Facilities)</div>
+            
+            <div class='legend-scale'>
+            <ul class='legend-labels'>
+            <li><span style= width:400px;font-size:110%;text-align:left;>Per Capita Income Color Scale:</span></li><br>
+            <li><span style='background:#a50026;'></span>$15,000</li>
+            <li><span style='background:#d62f27;'></span>$25,000</li>
+            <li><span style='background:#f46d43;'></span>$35,000</li>
+            <li><span style='background:#fdad60;'></span>$45,000</li>
+            <li><span style='background:#fee08b'></span>$55,000</li>
+            <li><span style='background:#feffbe;'></span>$65,000</li>
+            <li><span style='background:#d9ef8b;'></span>$75,000</li>
+            <li><span style='background:#a5d86a;'></span>$85,000</li>
+            <li><span style='background:#66bd63;'></span>$95,000</li>
+            <li><span style='background:#199750'></span>$105,000</li>
+            <li><span style='background:#006837'></span>$115,000</li>
+        </ul>
+        </div>
+        <div class='legend-source'>Sources: <a href="http://www.census.gov">US Census Data</a>, <a href="http://www.google.com">Google</a></div>
+        </div>
+        
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        <style type='text/css'>
+        .my-legend .legend-title {{
+            text-align: left;
+            margin-bottom: 8px;
+            font-weight: bold;
+            font-size: 150%;
+            }}
+        .my-legend .legend-marker {{
+            text-align: left;
+            margin-bottom: 10px;
+            font-size: 80%;
+            }}
+        .my-legend .legend-scaledsc {{
+            font-size: 90%;
+            padding: 0px;
+            }}
+    
+        .my-legend .legend-scale ul {{
+            margin-bottom: 0;
+            padding: 0;
+            float: left;
+            list-style: none;
+            }}
+        .my-legend .legend-scale ul li {{
+            display: block;
+            width: 40px;
+            margin-bottom: 0px;
+            float: left;
+            text-align: center;
+            font-size: 80%;
+            list-style: none;
+            }}
+        .my-legend ul.legend-labels li span {{
+            display: block;
+            float: left;
+            height: 15px;
+            width: 40px;
+            }}
+        .my-legend ul.legend-labels span {{
+            display: block;
+            height: 15px;
+            width: 80px;
+            float: left;
+            }}
+            
+            
+        .my-legend .legend-source {{
+            padding-top:10px;
+            font-size: 70%;
+            color: #999;
+            clear: both;
+            }}
+        .my-legend a {{
+            color: #777;
+            }}
+        </style>
+
+
+
+
+"""
